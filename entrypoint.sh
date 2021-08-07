f3bb80b11400fbbdd4093daec0edaee5a1a7c39c
koolwiki <20160955+koolwiki@users.noreply.github.com>
2021-08-07 20:21:11 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 cd /goflyway-heroku
 
-wget -O goflyway.tar.gz https://github.com/coyove/goflyway/releases/download/v1.2.0a/goflyway_linux_amd64.tar.gz
+wget -O goflyway.tar.gz https://github.com/coyove/goflyway/releases/download/2.0.0rc1/goflyway_linux_amd64.tar.gz
 tar -zxf goflyway.tar.gz
 #cd /goflyway/goflyway_linux_amd64
 chmod +x goflyway 
